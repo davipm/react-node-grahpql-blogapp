279363ad50b3c49534f164057712bb3c672a3277
davip <davi.p.m94@gmail.com>
2020-04-18 14:15:00 -0300

    test

--- config/db.js
+++ config/db.js
@@ -7,7 +7,7 @@ class Database {
         useNewUrlParser: true,
         useUnifiedTopology: true
       });
-      console.log('Database connected');
+      console.log('Database connected success');
     } catch (error) {
       console.log(error);
     }
