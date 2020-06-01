6d4330ae2d6fabb0e7dcb86cf8232e213706d0ba
davi <davi.p.m94@gmail.com>
2020-06-01 10:11:56 -0300

    create mutation get post and create post

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -6,18 +6,51 @@ const postsResolvers = {
   Query: {
     async getPosts() {
       try {
-        return await Post.find();
+        const posts = await Post.find().sort({ createdAt: -1 });
+        return posts;
       } catch (err) {
         throw new Error(err);
       }
     },
 
     async getPost(_, { postId }) {
-      //
+      try {
+        const post = await Post.findById(postId);
+        if (post) {
+          return post;
+        } else {
+          throw new Error("Post not found");
+        }
+      } catch (error) {
+        throw new Error(error);
+      }
     },
   },
 
-  Mutation: {},
+  Mutation: {
+    async createPost(_, { body }, context) {
+      const user = checkAuth(context);
+
+      if (body.trim() === "") {
+        throw new Error("Post body must not be empty");
+      }
+
+      const newPost = new Post({
+        body,
+        user: user.id,
+        username: user.username,
+        createdAt: new Date().toISOString(),
+      });
+
+      const post = await newPost.save();
+
+      context.pubSub.publish("NEW_POST", {
+        newPost: post,
+      });
+
+      return post;
+    },
+  },
 };
 
 export default postsResolvers;
