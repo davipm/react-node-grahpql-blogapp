f5ffeb8ef73ba46d703606321242992745338192
davi <davi.p.m94@gmail.com>
2020-06-01 10:54:33 -0300

    define resolves

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,12 +1,25 @@
 import postsResolvers from "./posts";
 import usersResolvers from "./users";
+import commentResolver from "./comments";
 
 const resolvers = {
+  Post: {
+    likeCount: (parent) => parent.likes.length,
+    commentCount: (parent) => parent.comments.length,
+  },
+
   Query: {
     ...postsResolvers.Query,
   },
+
   Mutation: {
     ...usersResolvers.Mutation,
+    ...usersResolvers.Mutation,
+    ...commentResolver.Mutation,
+  },
+
+  Subscription: {
+    ...postsResolvers.Subscription,
   },
 };
 
