5c54d88c492013eb76a2cb267031fb962f78b6fb
davi <davi.p.m94@gmail.com>
2020-06-01 10:15:00 -0300

    create mutation delete post

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -50,6 +50,22 @@ const postsResolvers = {
 
       return post;
     },
+
+    async deletePost(_, { postId }, context) {
+      const user = checkAuth(context);
+
+      try {
+        const post = await Post.findById(postId);
+        if (user.username === post.username) {
+          await post.delete();
+          return 'Post deleted successfully'
+        } else {
+          throw new AuthenticationError('Action not allowed');
+        }
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
   },
 };
 
