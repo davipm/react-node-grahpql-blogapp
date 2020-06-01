c894b69a84f345a4f17a14beb8fdf099e3b8a31f
davi <davi.p.m94@gmail.com>
2020-06-01 10:23:06 -0300

    create subscribe new post

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -119,6 +119,12 @@ const postsResolvers = {
       }
     },
   },
+
+  Subscription: {
+    newPost: {
+      subscribe: (_, __, { pubsub }) => pubsub.asyncIterator("NEW_POST"),
+    },
+  },
 };
 
 export default postsResolvers;
