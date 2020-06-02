1cd376dcd4ffe2ce8e8b7e581f5c919b95e9de4c
davi <davi.p.m94@gmail.com>
2020-06-01 21:33:09 -0300

    add apollo client

---
+++ client/src/services/apolloClient.js
@@ -0,0 +1,24 @@
+import ApolloClient from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { createHttpLink } from "apollo-link-http";
+import { setContext } from "apollo-link-context";
+
+const httpLink = new createHttpLink({
+  uri: "http://localhost:4000",
+});
+
+const authLink = setContext(() => {
+  const token = localStorage.getItem("jwtToken");
+  return {
+    headers: {
+      Authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+export default client;
