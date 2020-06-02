ebc3e13ef44ef98ca5ae1019aa0cb4032d1d43e1
davi <davi.p.m94@gmail.com>
2020-06-01 21:42:40 -0300

    add posts fetch

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Routes from "./routes";
 
 function App() {
   return (
-    <div className="App">
+    <div id="app">
       <Container>
         <Routes />
       </Container>
---
+++ client/src/components/Popup/Popup.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Popup(props) {
+  return (
+    <div>
+      <p>Popup</p>
+    </div>
+  );
+}
+
+export default Popup;
